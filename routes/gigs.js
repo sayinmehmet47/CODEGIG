e95086ff8daa118d46927a900c7b0c2bfff91a7f
unknown <sayinmehmet47@gmail.com>
2021-11-28 22:07:49 +0200

    create route added

--- routes/gigs.js
+++ routes/gigs.js
@@ -8,4 +8,16 @@ router.get("/", function (req, res) {
     .catch((err) => console.log(err));
 });
 
+router.get("/add", function (req, res) {
+  Gig.create({
+    title: "Mehmet",
+    technologies: "NodeJS,MongoDb,PostgrSql",
+    description:
+      "Note, from the usage of await in the snippet above, that save is an asynchronous method. In fact, almost every Sequelize method is asynchronous; build is one of the very few exceptions.",
+    contact_email: "sayinmehmet47@gmail.com",
+  })
+    .then((gigs) => res.redirect("/gigs"))
+    .catch((err) => console.log(err));
+});
+
 module.exports = router;
